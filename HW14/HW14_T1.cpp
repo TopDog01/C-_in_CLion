4cb59562163ddda39dd1f85a8a005e71f9b32c82
Yuri Konev <crazy_clown93@mail.ru>
2025-07-13 21:37:17 +0300

    homework 14

--- HW14/HW14_T1.cpp
+++ HW14/HW14_T1.cpp
@@ -1,49 +1,56 @@
 #include <iostream>
 
 int main() {
-    // Initialize arrays
-    int utensils[2][12] = { {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4},
-                            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
 
-    int plates[2][12] = { {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3},
-                          {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
+    int cutlery[2][6] = {
+            {3, 3, 3, 3, 3, 3},
+            {3, 3, 3, 3, 3, 3}
+    };
 
-    int chairs[2][12] = { {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                          {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
+
+    int plates[2][2] = {
+            {2, 1},
+            {2, 1}
+    };
+
+
+    int chairs[2][6] = {
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1}
+    };
 
 
     chairs[0][4] += 1;
 
 
-    utensils[0][2] -= 1;
+    cutlery[1][2] -= 1;
 
 
-    utensils[0][10] -= 1;
-    utensils[0][2] += 1;
+    cutlery[0][0] -= 1;
+    cutlery[1][2] += 1;
 
 
-    plates[0][10] -= 1;
+    plates[0][1] -= 1;
 
 
-    std::cout << "Utensils:" << std::endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 12; j++) {
-            std::cout << utensils[i][j] << " ";
+    std::cout << "Cutlery (forks, spoons, knives):\n";
+    for (int i = 0; i < 2; ++i) {
+        std::cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < 6; ++j) {
+            std::cout << cutlery[i][j] << " ";
         }
         std::cout << std::endl;
     }
 
-    std::cout << "Plates:" << std::endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 12; j++) {
-            std::cout << plates[i][j] << " ";
-        }
-        std::cout << std::endl;
+    std::cout << "\nPlates (regular, dessert):\n";
+    for (int i = 0; i < 2; ++i) {
+        std::cout << "Row " << i + 1 << ": " << plates[i][0] << " regular, " << plates[i][1] << " dessert\n";
     }
 
-    std::cout << "Chairs:" << std::endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 12; j++) {
+    std::cout << "\nChairs:\n";
+    for (int i = 0; i < 2; ++i) {
+        std::cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < 6; ++j) {
             std::cout << chairs[i][j] << " ";
         }
         std::cout << std::endl;
